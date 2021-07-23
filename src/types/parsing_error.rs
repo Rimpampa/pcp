7fe1ec9bcb32c4409cd30014cc8e8647c51a3a1b
Riccardo Ripanti <riccardo.ripanti01@gmail.com>
2021-07-23 20:52:17 +0200

    impl Display for ParsingError

--- src/types/parsing_error.rs
+++ src/types/parsing_error.rs
@@ -1,6 +1,7 @@
 use super::{OpCode, OptionCode};
+use std::fmt;
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub enum ParsingError {
     /// The parsed value (1st) is not an opcode
     NotAnOpCode(u8),
@@ -18,8 +19,36 @@ pub enum ParsingError {
     VersionNotSupported(u8),
     /// The R field of the response message header is not 1
     NotAResponse,
+    /// The R field of the request message header is not 0
+    NotARequest,
     /// The filter option prefix is invalid
     InvalidPrefix(u8),
     /// The option code (2nd) is not valid for that opcode (1st)
     InvalidOption(OpCode, OptionCode),
+    /// The constructed packet is too big
+    PacketTooBig(usize),
+}
+
+impl fmt::Display for ParsingError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        use ParsingError::*;
+        match self {
+            NotAnOpCode(c) => write!(f, "The value {} is not a valid opcode", c),
+            NotAnOptionCode(c) => write!(f, "The value {} is not a valid option code", c),
+            NotAResultCode(c) => write!(f, "The value {} is not a valid result code", c),
+            NotAProtocolNumber(n) => write!(f, "The value {} is not a valid protocol number", n),
+            InvalidSliceLength(l) => write!(f, "The the slice as an invalid length of {}", l),
+            InvalidOptionLength(o, l) => {
+                write!(f, "A length of {} is invalid for the option {:?}", l, o)
+            }
+            VersionNotSupported(v) => write!(f, "Unsupported version {}", v),
+            NotAResponse => write!(f, "The request flag is set thus it's not a response"),
+            NotARequest => write!(f, "The request flag is not set thus it's not a request"),
+            InvalidPrefix(p) => write!(f, "Invalid prefix length of {}", p),
+            InvalidOption(o, p) => {
+                write!(f, "The option {:?} is not valid for the opcode {:?}", o, p)
+            }
+            PacketTooBig(l) => write!(f, "The generated packet of size {} is too big", l),
+        }
+    }
 }
