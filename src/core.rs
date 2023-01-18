e582f64a1d926b8ca15f7cfae230d7b3ba7ea2db
Riccardo Ripanti <riccardo.ripanti01@gmail.com>
2023-01-18 20:48:30 +0100

    Add options module
    This module kind of merges the headers and payloads
    representing the data in a more idiomatic way

--- src/core.rs
+++ src/core.rs
@@ -23,6 +23,9 @@ pub use protocols::ProtocolNumber;
 
 mod util;
 
+pub mod option;
+pub use option::Option;
+
 /// Maximum size a PCP UDP packet can have
 pub const MAX_PACKET_SIZE: usize = 1100;
 
