a14cc2c4d163b0dfc025613ce8b23f9eb5196f29
Riccardo Ripanti <riccardo.ripanti01@gmail.com>
2023-02-12 16:19:44 +0100

    Add core module
    The core module will contain all the types and methods needed to make
    a compliant PCP client and (hopefully) server

---
+++ src/core.rs
@@ -0,0 +1,23 @@
+//! This module defines all the structs representing PCP response and request
+//! packets and the fields contained in them.
+//!
+//! TODO
+
+/// Maximum size a PCP UDP packet can have
+pub const MAX_PACKET_SIZE: usize = 1100;
+
+/// IANA assigned UDP port number for PCP servers
+///
+/// A PCP servers use this UDP port for:
+/// - listening for unicast clients requests
+/// - sending multicast notifications to clients
+pub const SERVER_PORT: u16 = 5351;
+
+/// IANA assigned UDP port number for PCP client
+///
+/// PCP clients listen on this UDP port for servers
+/// multicast notifications
+///
+/// **Note** that on transmission the clients can
+/// use any UDP port
+pub const CLIENT_PORT: u16 = 5350;
